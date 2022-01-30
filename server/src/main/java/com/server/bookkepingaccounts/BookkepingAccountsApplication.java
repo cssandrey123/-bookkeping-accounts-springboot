e9ab58ee02ba976fd19823d258f7ded8635fd9d5
Ispas Andrei <andrei@devplant.ro>
2022-01-30 21:34:59 +0200

    added spring-boot

---
+++ server/src/main/java/com/server/bookkepingaccounts/BookkepingAccountsApplication.java
@@ -0,0 +1,13 @@
+package com.server.bookkepingaccounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookkepingAccountsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BookkepingAccountsApplication.class, args);
+  }
+
+}
---
+++ server/src/test/java/com/server/bookkepingaccounts/BookkepingAccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.bookkepingaccounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookkepingAccountsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
